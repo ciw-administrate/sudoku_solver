0046b48e37bf71b38dd3cfbbe6b973cc45b31e64
Calum Whyte <ciw@getadministrate.com>
2019-12-01 15:49:23 +0000

    confirm that the sum of all numbers on a given row, is 45

--- app/sudoku_solver.py
+++ app/sudoku_solver.py
@@ -1,4 +1,11 @@
+from typing import List
+
+
 class SudokuSolver(object):
     @staticmethod
-    def solve() -> bool:
-        return True
+    def sum_of_row(solution: List[List[int]], row_number: int) -> int:
+        row: List[int] = solution[row_number]
+        total: int = 0
+        for number in row:
+            total += number
+        return total
