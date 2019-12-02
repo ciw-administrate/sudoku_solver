f8e7fdc523720f5a97f5a4e8c360d853516979ab
Calum Whyte <ciw@getadministrate.com>
2019-12-02 13:03:49 +0000

    add a function to ensure that a row contains each number from 1 to 9

--- app/sudoku_solver.py
+++ app/sudoku_solver.py
@@ -12,11 +12,22 @@ class SudokuSolver(object):
 
     @staticmethod
     def sum_of_column(solution: List[List[int]], column_number: int) -> int:
+        print("row 0 = " + str(solution[0]))
         column: List[int] = []
-        row: List[int]
-        for i in range(len(solution)):
-            column.append(solution[i][column_number])
+        for row_number in range(len(solution)):
+            column.append(solution[row_number][column_number])
         total: int = 0
         for number in column:
             total += number
         return total
+
+    @staticmethod
+    def sort_row(solution: List[List[int]], row_number: int) -> List[int]:
+        row: List[int] = solution[row_number].copy()
+        sorted_row: List[int] = []
+        for number in range(1, 10):
+            if number in row:
+                number_index = row.index(number)
+                temp_num = row.pop(number_index)
+                sorted_row.append(temp_num)
+        return sorted_row
